55cefbb59c9156f2542b272e75b08ddd2947331c
xrotwang <xrotwang@googlemail.com>
2020-03-17 17:29:57 +0100

    address glottolog/glottolog#459

--- src/pyglottolog/admin_commands/updatesources.py
+++ src/pyglottolog/admin_commands/updatesources.py
@@ -10,10 +10,23 @@ def run(args):
     langs = args.repos.languoids_by_code()
     updated = []
     sources = collections.defaultdict(set)
+    glrefs = set()
+
+    for entry in args.repos.bibfiles['hh.bib'].iterentries():
+        for lang in entry.languoids(langs)[0]:
+            sources[lang.id].add('{0}:{1}'.format('hh', entry.key))
+            if entry.fields.get('glottolog_ref_id'):
+                glrefs.add(entry.fields['glottolog_ref_id'])
+
     for bib in args.repos.bibfiles:
+        if bib.id == 'hh':
+            continue
         for entry in bib.iterentries():
-            for lang in entry.languoids(langs)[0]:
-                sources[lang.id].add('{0}:{1}'.format(bib.id, entry.key))
+            # If language associations have already been read from an equivalent record in hh.bib,
+            # we disregard the entry.
+            if entry.fields.get('glottolog_ref_id') not in glrefs:
+                for lang in entry.languoids(langs)[0]:
+                    sources[lang.id].add('{0}:{1}'.format(bib.id, entry.key))
 
     for gc, refs in sources.items():
         if refs != set(r.key for r in langs[gc].sources):
