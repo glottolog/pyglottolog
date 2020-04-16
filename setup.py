a2660577de4fec6db15509ca88f77ba7d07d6b7b
xrotwang <xrotwang@googlemail.com>
2020-04-16 08:06:46 +0200

    release 3.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyglottolog',
-    version='3.1.1.dev0',
+    version='3.2.0',
     author='Robert Forkel',
     author_email='forkel@shh.mpg.de',
     description='python package for glottolog data curation',
