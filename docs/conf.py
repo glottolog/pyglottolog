ae5b38f0f32168e835d22898cfbc98c15aa20bc0
Robert Forkel <xrotwang@googlemail.com>
2024-10-25 08:29:54 +0200

    release 3.14.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Robert Forkel'
 author = 'Robert Forkel'
 
 # The full version, including alpha/beta/rc tags
-release = '3.13.1.dev0'
+release = '3.14.0'
 
 
 # -- General configuration ---------------------------------------------------
